180f75d30d5d2a1873753947e63e69651c60f572
Melusi Mkhize <msstudymail@gmail.com>
2023-03-16 16:43:02 -0700

    another fix of bugs

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <std.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
-int_putchar(char c);
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
-void *_calloc(unsigned int memb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *_realloc(void *ptr, unsignedmint old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
